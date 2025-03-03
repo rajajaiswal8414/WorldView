9c24ff5fe685dc977778e8249c286ed7256c67c5
rajajais8414 <143202200+rajajaiswal8414@users.noreply.github.com>
2025-03-03 16:11:17 +0530

    Fixed Netlify routing

--- src/script.js
+++ src/script.js
@@ -19,7 +19,7 @@ function renderCountries(data) {
     data.forEach((country) => {
         const countryCard = document.createElement('a');
         countryCard.classList.add('xs:m-0', 'mx-14', 'shadow-md', 'h-auto', 'max-w-xs', 'hover:scale-105', 'transition', 'duration-300', 'ease-in-out', 'block', 'bg-white', 'dark:bg-darkBlue');
-        countryCard.href = `/API_Project/country.html?name=${country.name.common}`
+        countryCard.href = `/country.html?name=${country.name.common}`
 
         countryCard.innerHTML = `
                 <img class="w-full" src="${country.flags.svg}" alt="${country.name.common} flag" />
